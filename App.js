d3e66a062a43721f2d1b6c07b867547e3315934d
tthandb <tthandb@hotmail.com>
2020-07-28 05:31:35 +0700

    milestone 3 E

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import moment from "moment";
+import { Card, Button } from "react-native-elements";
+import { Icon } from "react-native-elements";
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+  const getNews = async () => {
+    const response = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=34fef9bfa512481681f9a6897ec764b3"
+    );
+    const jsonData = await response.json();
+    setArticles(jsonData.articles);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getNews();
+  }, []);
+  if (loading)
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" loading={loading} />
+      </View>
+    );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Card title={articles[0].title} image={{ uri: articles[0].urlToImage }} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  containerFlex: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  header: {
+    height: 30,
+    width: "100%",
+    backgroundColor: "pink",
+  },
+  row: {
+    flexDirection: "row",
+  },
+  label: {
+    fontSize: 16,
+    color: "black",
+    marginRight: 10,
+    fontWeight: "bold",
+  },
+  info: {
+    fontSize: 16,
+    color: "grey",
   },
 });
