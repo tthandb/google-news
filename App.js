b763e7edca0deb385540c036729856be32d374c6
tthandb <tthandb@gmail.com>
2020-07-30 23:15:17 +0700

    done milestone 4

--- App.js
+++ App.js
@@ -1,19 +1,64 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 import moment from "moment";
 import { Card, Button } from "react-native-elements";
 import { Icon } from "react-native-elements";
+const filterForUniqueArticles = (arr) => {
+  const cleaned = [];
+  arr.forEach((itm) => {
+    let unique = true;
+    cleaned.forEach((itm2) => {
+      const isEqual = JSON.stringify(itm) === JSON.stringify(itm2);
+      if (isEqual) unique = false;
+    });
+    if (unique) cleaned.push(itm);
+  });
+  return cleaned;
+};
 export default function App() {
   const [loading, setLoading] = useState(true);
   const [articles, setArticles] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
   const getNews = async () => {
     const response = await fetch(
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=34fef9bfa512481681f9a6897ec764b3"
     );
     const jsonData = await response.json();
-    setArticles(jsonData.articles);
+    /* setArticles(
+      jsonData.articles.filter((article) => article.urlToImage !== null)
+    );*/
+    const newArticleList = filterForUniqueArticles(
+      articles.concat(jsonData.articles)
+    );
+    setArticles(newArticleList.filter((article)=>article.urlToImage!==null));
+    setPageNumber(pageNumber + 1);
     setLoading(false);
+  };
+
+  const renderItem = ({ item }) => {
+    const article = item;
+    return (
+      <Card title={article.title} image={{ uri: article.urlToImage }}>
+        <View style={styles.row}>
+          <Text style={styles.label}>Source</Text>
+          <Text style={styles.info}>{article.source.name}</Text>
+        </View>
+        <Text style={{ marginBottom: 10 }}>{article.content}</Text>
+        <View style={styles.row}>
+          <Text style={styles.label}> Published</Text>
+          <Text style={styles.info}>
+            {moment(article.publishedAt).format("LLL")}
+          </Text>
+        </View>
+        <Button icon={<Icon />} title="Read more" backgroundColor="#03A9F4" />
+      </Card>
+    );
   };
   useEffect(() => {
     getNews();
@@ -24,9 +69,23 @@ export default function App() {
         <ActivityIndicator size="large" loading={loading} />
       </View>
     );
+
   return (
     <View style={styles.container}>
-      <Card title={articles[0].title} image={{ uri: articles[0].urlToImage }} />
+      <View style={styles.row}>
+        <Text style={styles.label}>Articles Count:</Text>
+        <Text style={styles.info}>{articles.length}</Text>
+      </View>
+      <FlatList
+        data={articles}
+        onEndReached={getNews}
+        onEndReachedThreshold={1}
+        keyExtractor={(data, index) => {
+          console.log(index);
+          return index.toString();
+        }}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
